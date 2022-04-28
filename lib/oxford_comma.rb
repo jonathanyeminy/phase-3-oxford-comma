c023263902377a6445c2c4734893866624a6ed8e
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-04-28 13:23:47 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+    new_array = []
+    counter = 1
+    array.each do |string|
+        if counter == 1
+        new_array << string
+        counter +=1
+        elsif (counter == array.length && counter == 2)
+            new_array << " and #{string}"
+        elsif counter != (array.length)
+            new_array << ", #{string}"
+            counter +=1
+        else 
+            new_array << ", and #{string}"
+        end
+    end
+    new_array.join
 end
